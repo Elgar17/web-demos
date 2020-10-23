c032f600ea15325c61d7752c7c06b88f4ccac4ed
Elgar17 <elgara@163.com>
2020-10-23 16:31:09 +0800

    add simple player

---
+++ 04player/js/utils.js
@@ -0,0 +1,14 @@
+
+// 节流函数
+var throttle = function (func, delay) {
+    var prev = Date.now();
+    return function () {
+        var context = this;
+        var args = arguments;
+        var now = Date.now();
+        if (now - prev >= delay) {
+            func.apply(context, args);
+            prev = Date.now();
+        }
+    }
+}
